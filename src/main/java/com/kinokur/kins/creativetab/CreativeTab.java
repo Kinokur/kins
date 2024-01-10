fab6d82a8827ac89770602f087c417f8c83907a8
Daniil Mafin <daniilmafin534@gmail.com>
2024-01-10 20:11:59 +0300

    add new block and items

--- src/main/java/com/kinokur/kins/creativetab/CreativeTab.java
+++ src/main/java/com/kinokur/kins/creativetab/CreativeTab.java
@@ -1,5 +1,32 @@
-package com.kinokur.kins.item.creativetab;
+package com.kinokur.kins.creativetab;
+
+import com.kinokur.kins.Kins;
+import com.kinokur.kins.block.ModBlocks;
+import com.kinokur.kins.item.ModItems;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 
 public class CreativeTab {
-    
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Kins.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> ITEMS_TAB = CREATIVE_MODE_TABS.register("item_tab", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .title(Component.translatable("creativemodetab.kins.item_tab"))
+            .icon(() -> ModItems.TOPAZ.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+            output.accept(ModItems.TOPAZ.get());
+            output.accept(ModItems.RAW_TOPAZ.get());
+
+            output.accept(ModBlocks.TOPAZ_ORE.get());
+            output.accept(ModBlocks.TOPAZ_BLOCK.get());
+            }).build());
+
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
 }
--- src/main/java/com/kinokur/kins/item/ModItems.java
+++ src/main/java/com/kinokur/kins/item/ModItems.java
@@ -1,2 +1,23 @@
-package com.kinokur.kins.item;public class ModItems {
+package com.kinokur.kins.item;
+
+import com.kinokur.kins.Kins;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Kins.MOD_ID);
+
+    public static final RegistryObject<Item> TOPAZ =
+            ITEMS.register("topaz",() -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_TOPAZ =
+            ITEMS.register("raw_topaz",() -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
 }
